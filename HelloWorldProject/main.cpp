2837cf833363a4e58c67d0e3de3a65cfa863d4ba
Terry Nguyen <terry@terrehbyte.com>
2022-01-11 16:53:05 -0800

    Add reminder about saving

--- HelloWorldProject/main.cpp
+++ HelloWorldProject/main.cpp
@@ -4,7 +4,7 @@ int main()
 {
 	std::cout << "Welcome to the Australian Science Center." << std::endl;
 	std::cout << "We hope you've had a good time." << std::endl;
-	std::cout << "For your safety, save before exiting.." << std::endl;
+	std::cout << "Please make sure to save before committing." << std::endl;
 
 	std::cout << "Copyright (c) ASC" << std::endl;
 
